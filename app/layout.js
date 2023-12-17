98c2c69e2fd08844a4e171fb3c58d90c36d7420e
Antonio Santos <aj.santos128@gmail.com>
2023-12-17 15:50:07 -0400

    Redesigned page and migrated to app router

--- app/layout.js
+++ app/layout.js
@@ -6,8 +6,8 @@ export default function RootLayout({
     children,
 }) {
     return (
-        <html lang="en">
-            <body>{children}</body>
+        <html lang="en" className='h-full'>
+            <body className='h-full'>{children}</body>
         </html>
     )
 }
