741679957dbc9f4247fcf6961e29251d4b941b00
Antonio Santos <aj.santos128@gmail.com>
2023-12-18 20:14:10 -0400

    Migrate files to /src directory.

---
+++ src/app/layout.js
@@ -0,0 +1,23 @@
+import '@/styles/tailwind.css';
+import { Layout } from '@/components/Layout';
+import { Providers } from '@/app/providers';
+
+export default function RootLayout({
+  // Layouts must accept a children prop.
+  // This will be populated with nested layouts or pages
+  children,
+}) {
+  return (
+    <html lang="en" className='h-full antialiased' suppressHydrationWarning>
+      <body className='flex h-full bg-neutral-50 dark:bg-black'>
+        <div className='flex w-full'>
+          <Providers>
+            <Layout>
+              {children}
+            </Layout>
+          </Providers>
+        </div>
+      </body>
+    </html>
+  )
+}
