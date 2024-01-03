eb05631beb3af035e278918f2bb808b9d91d1c02
Antonio Santos <aj.santos128@gmail.com>
2024-01-02 20:23:47 -0400

    Update pages to use /api/og for OpenGraph images

--- src/app/thank-you/page.jsx
+++ src/app/thank-you/page.jsx
@@ -1,15 +1,22 @@
-import { SimpleLayout } from '@/components/SimpleLayout'
+import { SimpleLayout } from '@/components/SimpleLayout';
 
 export const metadata = {
   title: 'You’re subscribed',
   description: 'Thanks for subscribing to my newsletter.',
-}
+  openGraph: {
+    images: [
+      {
+        url: `${process.env.NEXT_PUBLIC_BASE_URL}/api/og?title=${title}`,
+      },
+    ],
+  },
+};
 
 export default function ThankYou() {
   return (
     <SimpleLayout
-      title="Thanks for subscribing."
-      intro="I’ll send you an email any time I publish a new blog post, release a new project, or have anything interesting to share that I think you’d want to hear about. You can unsubscribe at any time, no hard feelings."
+      title='Thanks for subscribing.'
+      intro='I’ll send you an email any time I publish a new blog post, release a new project, or have anything interesting to share that I think you’d want to hear about. You can unsubscribe at any time, no hard feelings.'
     />
-  )
+  );
 }
